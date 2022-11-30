ed13dead598819147400532ca090fc8c936d3176
pkostic-dev <23319698+pkostic-dev@users.noreply.github.com>
2022-11-30 19:12:56 +0100

    test 1

---
+++ Wraith_Hunter/ghost.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+
+func _ready():
+	var tween := get_tree().create_tween()
+	tween.tween_property(self, "position", Vector3(10, position.y, position.z), 1)
+	tween.tween_property(self, "position", Vector3(-10, position.y, position.z), 1)
+	tween.set_loops(20)
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		$AudioStreamPlayer3D.play()
