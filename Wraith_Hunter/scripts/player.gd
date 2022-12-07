0c5aa98d5fa3f1042dd9b3549df038849491269c
pkostic-dev <23319698+pkostic-dev@users.noreply.github.com>
2022-12-07 11:22:51 +0100

    Ajout test gyroscope

---
+++ Wraith_Hunter/scripts/player.gd
@@ -0,0 +1,60 @@
+extends Node3D
+
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	var acc = Input.get_accelerometer()
+	var grav = Input.get_gravity()
+	var mag = Input.get_magnetometer()
+	var gyro = Input.get_gyroscope()
+
+	$"%Acc".text = "Accelometer : " + str(acc) 
+	$"%Grav".text = "Gravity : " + str(grav)
+	$"%Mag".text = "Magnetometer : " + str(mag)
+	$"%Gyro".text = "Gyroscope : " + str(gyro)
+	
+	var camera = $"%Camera3D"
+	var new_basis = rotate_by_gyro(gyro, camera.transform.basis, delta).orthonormalized()
+	print("Gyroscope :\t\t\t", str(gyro))
+	print("Camera Basis :\t\t", str(camera.transform.basis))
+	print("New Basis :\t\t\t", str(new_basis))
+	
+	camera.transform.basis = drift_correction(new_basis, grav)
+	print("Gravity :\t\t\t", str(grav))
+	print("Drift Correction :\t", str(camera.transform.basis))
+	print()
+
+
+# This function takes our gyro input and update an orientation matrix accordingly
+# The gyro is special as this vector does not contain a direction but rather a
+# rotational velocity. This is why we multiply our values with delta.
+func rotate_by_gyro(p_gyro, p_basis, p_delta):
+	var rotate = Basis()
+	
+	rotate = rotate.rotated(p_basis.x, -p_gyro.x * p_delta)
+	rotate = rotate.rotated(p_basis.y, p_gyro.y * p_delta)
+	rotate = rotate.rotated(p_basis.z, -p_gyro.z * p_delta)
+	
+	return rotate * p_basis
+
+
+# This function corrects the drift in our matrix by our gravity vector 
+func drift_correction(p_basis, p_grav):
+	# as always, make sure our vector is normalized but also invert as our gravity points down
+	var real_up = -p_grav.normalized()
+	
+	# start by calculating the dot product, this gives us the cosine angle between our two vectors
+	var dot = p_basis.y.dot(real_up)
+	
+	# if our dot is 1.0 we're good
+	if (dot < 1.0):
+		# the cross between our two vectors gives us a vector perpendicular to our two vectors
+		var axis = p_basis.y.cross(real_up).normalized()
+		var correction = Basis(axis, acos(dot))
+		p_basis = correction * p_basis
+	
+	return p_basis
+
