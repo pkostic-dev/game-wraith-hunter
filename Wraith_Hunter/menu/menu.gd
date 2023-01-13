6499eda3126a67752d3082aee4b3625571bbbdef
Clarahgi <clara.harzo@gmail.com>
2023-01-13 13:29:32 +0100

    Revert "Menu creation"
    This reverts commit e57485bb57e1515b2c98f287fa37cc37abf5fd98.

--- Wraith_Hunter/menu/menu.gd
+++ Wraith_Hunter/menu/menu.gd
@@ -1,48 +1,14 @@
 extends Node
-var swipe_start = null
-var minimum_drag = 100
-var largeur_ecran = 1150
+
+var side_a = $SideA
+var side_b = $SideB
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
 	pass
-
-# Called every frame. 'delta' is the 
-# elapsed time since the previous frame.
-func _process(_delta):
-	pass
-
- # Affichage du menu settings
-func _on_settings_pressed():
-	var scene = "res://menu/menu_settings.tscn"
-	var err = get_tree().change_scene_to_file(scene)
-	if err != OK:
-		printerr("La scene %s n'existe pas!" % scene)
-
-# Prise en compte du swipe
-func _unhandled_input(event):
-	if event is InputEventScreenTouch:
-		if event.pressed:
-			swipe_start = event.get_position()
-		else:
-			_calculate_swipe(event.get_position())
-			
-
-func _calculate_swipe(swipe_end):
-	if swipe_start == null: 
-		return
-	var swipe = swipe_end - swipe_start
-	if abs(swipe.x) > minimum_drag:
-		if swipe.x > 0:
-			_right()
-		else:
-			_left()
-
- # Action au swipe right
-func _right():
-	$Container/StartGameContainer.position.x -= largeur_ecran
-
- # Action au swipe left
-func _left():
-	$Container/StartGameContainer.position.x += largeur_ecran
-
