8474b99f1d9e6ebeeed71bae5aa268674674d503
pkostic-dev <23319698+pkostic-dev@users.noreply.github.com>
2023-01-17 23:28:36 +0100

    Add raycast, simulated gyroscope for PC

--- Wraith_Hunter/hunting/player.gd
+++ Wraith_Hunter/hunting/player.gd
@@ -4,6 +4,8 @@ var gravity:Vector3
 var gyroscope:Vector3
 
 @onready var head := $Head
+@onready var raycast := $Head/RayCast3D
+@onready var raycast_debug := $Head/RayCast3D/RayCastDebug
 
 func _process(delta):
 	gravity   = Input.get_gravity()
@@ -12,6 +14,21 @@ func _process(delta):
 	var new_basis = rotate_by_gyro(gyroscope, head.transform.basis, delta).orthonormalized()
 
 	head.transform.basis = new_basis
+	
+	# Simulate gyroscope for debugging on PC
+	if Input.is_action_pressed("ui_left"):
+		head.transform.basis = rotate_by_gyro(Vector3(0, 1, 0), head.transform.basis, delta).orthonormalized()
+	
+	if Input.is_action_pressed("ui_right"):
+		head.transform.basis = rotate_by_gyro(Vector3(0, -1, 0), head.transform.basis, delta).orthonormalized()
+
+
+func _physics_process(_delta):
+	# Change RayCastDebug color based on whether RayCast3D is colliding
+	if raycast.is_colliding():
+		raycast_debug.mesh.material.albedo_color = Color(1, 1, 1)
+	else:
+		raycast_debug.mesh.material.albedo_color = Color(0.1, 0.2, 0.6)
 
 
 func rotate_by_gyro(p_gyro, p_basis, p_delta):
