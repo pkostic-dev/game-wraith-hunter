04e2666fba912b5a6246a940ac75e520b2c2025c
pkostic-dev <23319698+pkostic-dev@users.noreply.github.com>
2023-02-02 03:43:25 +0100

    Update ghost/spawner, beta 17 reqs
    Merged ghost and test_ghost (not final), properly spawning ghosts, updated export enums to use args instead of string

--- Wraith_Hunter/hunting/ghost.gd
+++ Wraith_Hunter/hunting/ghost.gd
@@ -1,36 +1,86 @@
 extends Node3D
 
-@export var speed:float = .5
-@export_range(0, 30, 0.1, "suffix:s", "or_greater") var repeat_time_min:float = 7
-@export_range(0, 30, 0.1, "suffix:s", "or_greater") var repeat_time_max:float = 9
+enum Behavior {
+	IDLE,
+	SIDE_TO_SIDE,
+	ZIG_ZAG,
+	HOMING,
+}
 
-# TODO : make finding the player cleaner
-@onready var player := get_parent().get_node("Player")
-@onready var audio_stream_player_3D := $AudioStreamPlayer3D
+@export_enum("Idle", "SideToSide", "ZigZag", "Homing") var behavior := 3
+@export var health := 100.0
+@export var strength := 1.0
+@export var homing_speed := .5
+@export var side_to_side_speed := 10.0
+@export var move_during_capture := false
+@export var capture_speed_multiplier := 0.2
+@export_range(0, 30, 0.1, "suffix:s", "or_greater") var repeat_time_min := 7.0
+@export_range(0, 30, 0.1, "suffix:s", "or_greater") var repeat_time_max := 9.0
+
+var player:Node3D
+var original_position:Vector3
+
+@onready var growl_sound := $GrowlSound
 @onready var sound_timer := $SoundTimer
 @onready var ghost_emoji := $GhostEmoji
 
 
 func _ready():
 	# Play the sound
-	audio_stream_player_3D.play()
+	#growl_sound.play()
 	
 	# Start the timer for the first time
 	sound_timer.wait_time = randf_range(repeat_time_min, repeat_time_max)
 	sound_timer.start()
+	
+	# Find the player
+	var player_name := "Player"
+	var player_exists := get_parent().has_node(player_name)
+	if player_exists:
+		player = get_parent().get_node(player_name)
+	
+	if is_inside_tree():
+		original_position = position
 
 
 func _physics_process(delta):
+	match behavior:
+		Behavior.IDLE:
+			pass
+		Behavior.SIDE_TO_SIDE:
+			_move_side_to_side(delta)
+		Behavior.ZIG_ZAG:
+			pass
+		Behavior.HOMING:
+			_move_home_in(delta)
+
+
+func _move_home_in(delta):
 	if player:
 		var player_direction = (player.position - position).normalized()
-		global_translate(speed * player_direction * delta)
+		global_translate(homing_speed * player_direction * delta)
+
+
+# TODO : Update for _physics_process
+func _move_side_to_side(delta):
+#	var _tween := get_tree().create_tween()
+#	var _left = -10
+#	var _right = 10
+#	var _duration = 5
+#	var _loops = 100
+#
+#	_tween.tween_property(self, "position", Vector3(_right, position.y, position.z), _duration)
+#	_tween.tween_property(self, "position", Vector3(_left, position.y, position.z), _duration)
+#
+#	_tween.set_loops(_loops)
+	pass
 
 
 func _on_sound_timer_timeout():
-	if not audio_stream_player_3D.playing:
+	if not growl_sound.playing:
 		# Play the sound
-		audio_stream_player_3D.play()
-		# Change the GhostEmoji text color to green
+		growl_sound.play()
+		# DEBUG : Change the GhostEmoji text color to green
 		ghost_emoji.modulate = Color(.2, 1, .2)
 	
 	# Repeat the timer
@@ -38,6 +88,6 @@ func _on_sound_timer_timeout():
 	sound_timer.start()
 
 
-func _on_audio_stream_player_3d_finished():
-	# Reset the color to white
+func _on_growl_sound_finished():
+	# DEBUG : Reset the color to white
 	ghost_emoji.modulate = Color(1, 1, 1)
