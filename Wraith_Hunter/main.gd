381552ba5e9cefa6651b428c96d2498a5968bea8
pkostic-dev <23319698+pkostic-dev@users.noreply.github.com>
2023-02-21 05:20:01 +0100

    Update main.gd

--- Wraith_Hunter/main.gd
+++ Wraith_Hunter/main.gd
@@ -1,7 +1,5 @@
 extends Node
 
-var TEST_LEVEL := "res://tests/test_level.tscn"
-var TUTORIAL_SEQUENCE := "res://sequences/tutorial/tutorial_sequence.tscn"
 var SETUP_SEQUENCE := "res://sequences/setup/setup_sequence.tscn"
 
 func _ready():
