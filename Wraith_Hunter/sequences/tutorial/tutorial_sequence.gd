1ca93cf5570eeb1b0eaf28a09df594734da62b0f
pkostic-dev <23319698+pkostic-dev@users.noreply.github.com>
2023-02-21 06:04:37 +0100

    Update tutorial sequence

--- Wraith_Hunter/sequences/tutorial/tutorial_sequence.gd
+++ Wraith_Hunter/sequences/tutorial/tutorial_sequence.gd
@@ -25,6 +25,9 @@ var ghost_dead := false
 
 var OPENING_SEQUENCE := "res://sequences/opening/opening_sequence.tscn"
 
+const GHOST = preload("res://hunting/ghost.tscn") 
+var ghost
+
 # Sequence
 # 1. Play WRAITH_GROWL
 # 2. Play TUTORIAL_WRAITH
@@ -43,23 +46,32 @@ var OPENING_SEQUENCE := "res://sequences/opening/opening_sequence.tscn"
 
 
 func _ready():
+	# Create a ghost
+	var spawn_position = Vector3(randf_range(-9, 9), 0, 0)
+	ghost = await _spawn_ghost()
+	ghost.set_process(false)
+	ghost.behavior = ghost.Behavior.IDLE
+	ghost.default_behavior = ghost.Behavior.IDLE
+	ghost.original_position = spawn_position
+	ghost.position = spawn_position
+	
+	
 	_start_sequence()
 
 
 func _start_sequence():
 	$Player.can_aim = false
-	$Ghost.set_process(false)
 	
 	_play_sound(WRAITH_GROWL)
-	await $OpeningAudio.finished
-	await get_tree().create_timer(1.0).timeout
+	await get_tree().create_timer(2.0).timeout
+	$TutorialAudio.stop()
 	
 	_play_sound(TUTORIAL_WRAITH)
-	await $OpeningAudio.finished
+	await $TutorialAudio.finished
 	await get_tree().create_timer(1.0).timeout
 	
 	_play_sound(TUTORIAL_AIM)
-	await $OpeningAudio.finished
+	await $TutorialAudio.finished
 	await get_tree().create_timer(1.0).timeout
 	
 	$Player.can_aim = true
@@ -67,57 +79,57 @@ func _start_sequence():
 	$Player.can_aim = false
 	
 	_play_sound(TUTORIAL_CAPTURE_READY)
-	await $OpeningAudio.finished
+	await $TutorialAudio.finished
 	await get_tree().create_timer(1.0).timeout
 	
 	_play_sound(TUTORIAL_CAPTURE_START)
-	await $OpeningAudio.finished
+	await $TutorialAudio.finished
 	await get_tree().create_timer(1.0).timeout
 	
-	$Ghost.set_process(true)
-	await $Ghost.died
+	ghost.set_process(true)
+	await ghost.died
 	
 	_play_sound(TUTORIAL_CAPTURE_END)
-	await $OpeningAudio.finished
+	await $TutorialAudio.finished
 	await get_tree().create_timer(1.0).timeout
 	
 	_play_sound(TUTORIAL_WRAITH_ATTACK_1)
-	await $OpeningAudio.finished
+	await $TutorialAudio.finished
 	await get_tree().create_timer(1.0).timeout
 	
 	_play_sound(WRAITH_ATTACK)
-	await $OpeningAudio.finished
+	await $TutorialAudio.finished
 	await get_tree().create_timer(1.0).timeout
 	
 	_play_sound(TUTORIAL_WRAITH_ATTACK_2)
-	await $OpeningAudio.finished
+	await $TutorialAudio.finished
 	await get_tree().create_timer(1.0).timeout
 	
 	_play_sound(TUTORIAL_LOW_HEALTH_1)
-	await $OpeningAudio.finished
+	await $TutorialAudio.finished
 	await get_tree().create_timer(1.0).timeout
 	
 	_play_sound(LOW_HEALTH)
-	await $OpeningAudio.finished
+	await $TutorialAudio.finished
 	await get_tree().create_timer(1.0).timeout
 	
 	_play_sound(TUTORIAL_LOW_HEALTH_2)
-	await $OpeningAudio.finished
+	await $TutorialAudio.finished
 	await get_tree().create_timer(1.0).timeout
 	
 	Input.vibrate_handheld(400)
 	await get_tree().create_timer(1.0).timeout
 	
 	_play_sound(TUTORIAL_LOW_HEALTH_3)
-	await $OpeningAudio.finished
+	await $TutorialAudio.finished
 	await get_tree().create_timer(1.0).timeout
 	
 	_play_sound(TUTORIAL_WRAITH_MOVE)
-	await $OpeningAudio.finished
+	await $TutorialAudio.finished
 	await get_tree().create_timer(1.0).timeout
 	
 	_play_sound(TUTORIAL_PAUSE)
-	await $OpeningAudio.finished
+	await $TutorialAudio.finished
 	await get_tree().create_timer(1.0).timeout
 	
 	Global.goto_scene(OPENING_SEQUENCE)
@@ -129,3 +141,7 @@ func _play_sound(stream):
 	$TutorialAudio.stream = stream
 	$TutorialAudio.play()
 
+func _spawn_ghost():
+	var _ghost = GHOST.instantiate()
+	call_deferred("add_child", _ghost)
+	return _ghost
