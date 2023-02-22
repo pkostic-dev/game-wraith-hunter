e53dfc0f79e7be01ab95b157e3268bfa6da1c4da
pkostic-dev <23319698+pkostic-dev@users.noreply.github.com>
2023-02-22 05:42:14 +0100

    Fix screen cover

--- Wraith_Hunter/hunting/screen_cover.gd
+++ Wraith_Hunter/hunting/screen_cover.gd
@@ -2,8 +2,11 @@ extends CanvasLayer
 
 @onready var black_rect := $BlackRect
 
+
 func _on_toggle_cover_toggled(button_pressed):
 	if button_pressed:
-		black_rect.visible = true
+		$Polygon2D.visible = true
+		#black_rect.visible = true
 	else:
-		black_rect.visible = false
+		$Polygon2D.visible = false
+		#black_rect.visible = false
